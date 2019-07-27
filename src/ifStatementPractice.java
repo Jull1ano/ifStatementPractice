9133c2730ea45fb91664cd654ac42cbc7ff7bb01
Iulian <iulian181@gmail.com>
2019-07-27 11:27:40 +0300

    caca

---
+++ src/ifStatementPractice.java
@@ -0,0 +1,17 @@
+public class ifStatementPractice {
+    public static void main(String[] args) {
+
+        int num = 300;
+
+        if (num <= 300) {
+            if (num == 300) {
+                System.out.println("Este EGAL cu 300");
+            } else {
+                System.out.println("mai mic ca 300");
+            }
+
+        } else {
+            System.out.println("mai MARE ca 300");
+        }
+    }
+}
